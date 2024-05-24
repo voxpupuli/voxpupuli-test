c4d48b038ea989bede17204fa7526ce8ef333370
Tim Meusel <tim@bastelfreak.de>
2024-05-24 23:26:25 +0200

    Update tests for stringified facts

--- spec/facts_spec.rb
+++ spec/facts_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe 'override_facts' do
   let(:base_facts) do
     {
-      os: {
+      'os' => {
         'family' => 'RedHat',
         'name' => 'CentOS',
         'release' => {
@@ -18,7 +18,7 @@ describe 'override_facts' do
   describe 'no overrides' do
     let(:expected) do
       {
-        os: {
+        'os' => {
           'family' => 'RedHat',
           'name' => 'CentOS',
           'release' => {
@@ -36,7 +36,7 @@ describe 'override_facts' do
   describe 'with addition at the top level' do
     let(:expected) do
       {
-        os: {
+        'os' => {
           'family' => 'RedHat',
           'name' => 'CentOS',
           'release' => {
@@ -45,19 +45,19 @@ describe 'override_facts' do
             'minor' => '7'
           },
         },
-        ruby: {
+        'ruby' => {
           'sitedir' => '/usr/local/share/ruby/site_ruby',
         }
       }
     end
 
-    it { expect(override_facts(base_facts, ruby: {sitedir: '/usr/local/share/ruby/site_ruby'})).to eq(expected) }
+    it { expect(override_facts(base_facts, 'ruby' => { 'sitedir' => '/usr/local/share/ruby/site_ruby'})).to eq(expected) }
   end
 
   describe 'with deep merging' do
     let(:expected) do
       {
-        os: {
+        'os' => {
           'family' => 'RedHat',
           'name' => 'CentOS',
           'release' => {
@@ -69,13 +69,13 @@ describe 'override_facts' do
       }
     end
 
-    it { expect(override_facts(base_facts, os: {release: {minor: '8'}})).to eq(expected) }
+    it { expect(override_facts(base_facts, 'os' => { 'release' => {'minor' => '8'}})).to eq(expected) }
   end
 
   describe 'with strings' do
     let(:expected) do
       {
-        os: {
+        'os' => {
           'family' => 'RedHat',
           'name' => 'CentOS',
           'release' => {
@@ -87,7 +87,7 @@ describe 'override_facts' do
       }
     end
 
-    it { expect(override_facts(base_facts, os: {'release' => {minor: '8'}})).to eq(expected) }
+    it { expect(override_facts(base_facts, 'os' => {'release' => {minor: '8'}})).to eq(expected) }
   end
 end
 
@@ -131,7 +131,7 @@ describe 'add_facts_for_metadata' do
       it 'has systemd on Red Hat 7' do
         add_facts_for_metadata(metadata)
         facts = RspecPuppetFacts.with_custom_facts('redhat-7-x86_64', {
-          os: { 'family' => 'RedHat', 'release' => { 'major' => '7' } }
+          'os' => { 'family' => 'RedHat', 'release' => { 'major' => '7' } }
         })
         expect(facts['systemd']).to be true
       end
@@ -139,7 +139,7 @@ describe 'add_facts_for_metadata' do
       it 'has no systemd on Red Hat 6' do
         add_facts_for_metadata(metadata)
         facts = RspecPuppetFacts.with_custom_facts('redhat-6-x86_64', {
-          os: {'family' => 'RedHat', 'release' => { 'major' => '6' }}
+          'os' => {'family' => 'RedHat', 'release' => { 'major' => '6' }}
         })
         expect(facts['systemd']).to be false
       end
@@ -147,7 +147,7 @@ describe 'add_facts_for_metadata' do
       it 'has no systemd on openbsd' do
         add_facts_for_metadata(metadata)
         facts = RspecPuppetFacts.with_custom_facts('openbsd-6.4-x86_64', {
-          os: { 'family' => 'OpenBSD' }
+          'os' => { 'family' => 'OpenBSD' }
         })
         expect(facts['systemd']).to be false
       end
