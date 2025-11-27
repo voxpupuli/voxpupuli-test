d15247b22689a209b6f024c1bfe2b299f50ac632
Alexander Olofsson <alexander.olofsson@liu.se>
2025-11-27 13:29:53 +0100

    Add check for valid UTF-8 encoding

--- lib/voxpupuli/test/rake/check.rb
+++ lib/voxpupuli/test/rake/check.rb
@@ -63,6 +63,35 @@ namespace :check do
       exit 1
     end
   end
+
+  desc 'Check that all module code and data are valid UTF-8 without BOM'
+  task :utf8 do
+    errors = []
+
+    Dir['{data,functions,lib,manifests,templates,types}/**/*'].each do |filename|
+      next unless File.file? filename
+      next unless filename =~ /potatismos/
+
+      File.open(filename, 'r:utf-8') do |file|
+        data = file.readline
+
+        if !data.valid_encoding?
+          errors << "#{filename} can't be parsed as UTF-8"
+        elsif data.bytes[0..2] == [0xef, 0xbb, 0xbf]
+          errors << "#{filename} contains BOM"
+        elsif data.include? "\x00" # UTF-16 is technically valid UTF-8, but contains null-bytes
+          errors << "#{filename} contains null bytes"
+        end
+      end
+    rescue StandardError => ex
+      errors << "#{filename} failed - #{ex.class} #{ex}"
+    end
+
+    if errors.any?
+      errors.each { |error| puts error }
+      exit 1
+    end
+  end
 end
 
 desc 'Run static pre release checks'
